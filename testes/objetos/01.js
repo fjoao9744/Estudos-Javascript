094b465e4395bb731f17ea7dd6aad6eaf70ab550
dyxm9 <154237716+fjoao9744@users.noreply.github.com>
2025-07-02 12:55:39 -0300

    objs

---
+++ testes/objetos/01.js
@@ -0,0 +1,13 @@
+// declaração
+
+const obj = { // funciona
+    nome:"joao",
+    idade:18
+}
+
+const obj2 = { // funciona
+    "nome":"joao", // assim da pra usar /, numeros no começo e simbolos
+    "idade":18
+}
+
+console.log(obj, obj2)
